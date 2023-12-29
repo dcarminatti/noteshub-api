e08e294183b02a4a973ce686f90130e1b13a7990
dcarminatti <diogo.ca03@gmail.com>
2023-12-28 23:30:54 -0300

    feat: added the "express-async-errors" module, and configured the server to receive errors instantiated with the AppError class, in addition to also handling server errors

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("express-async-errors");
+
+const AppError = require("./utils/AppError");
 const express = require("express"),
     app = express(),
     PORT = 3000;
@@ -6,6 +9,22 @@ const routes = require("./routes");
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, req, res, next) => {
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            status: "error",
+            message: error.message,
+        });
+    }
+
+    console.error(error);
+
+    return res.status(500).json({
+        status: "error",
+        message: "Internal server error",
+    });
+});
 
 app.get("/message/:id", (req, res) => {
     res.send(`Id da mensagem: ${req.params.id}`);
