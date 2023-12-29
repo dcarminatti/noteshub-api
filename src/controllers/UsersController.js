9401326d6820dbf85845fe5ce7c011ddd2416ccb
dcarminatti <diogo.ca03@gmail.com>
2023-12-29 16:55:12 -0300

    feat: added route for http PUT "/update" method, created user information update algorithm and added encryption for user password

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,6 @@
 const AppError = require("../utils/AppError"),
-    sqliteConnection = require("../database/sqlite");
+    sqliteConnection = require("../database/sqlite"),
+    { hash } = require("bcryptjs");
 
 class UsersController {
     async create(req, res) {
@@ -15,12 +16,53 @@ class UsersController {
             throw new AppError("Este email já está em uso.");
         }
 
+        const hashedPassword = await hash(password, 8);
+
         await database.run(
             "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-            [name, email, password]
+            [name, email, hashedPassword]
         );
 
         return res.status(201).json({});
+    }
+
+    async update(req, res) {
+        const { name, email } = req.body,
+            { id } = req.params;
+
+        const database = await sqliteConnection(),
+            user = await database.get("SELECT * FROM users WHERE id = (?)", [
+                id,
+            ]);
+
+        if (!user) {
+            throw new AppError("Usuário não encontrado.");
+        }
+
+        const userWithUpdatedEmail = await database.get(
+            "SELECT * FROM users WHERE email = (?)",
+            [email]
+        );
+
+        if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+            throw new AppError("Este email já está em uso!");
+        }
+
+        user.name = name;
+        user.email = email;
+
+        await database.run(
+            `
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            updated_at = ?
+            WHERE id = ?
+        `,
+            [user.name, user.email, new Date(), id]
+        );
+
+        return res.json();
     }
 }
 
