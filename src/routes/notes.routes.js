3e4dae32b45a0cecd9f9ea6b52e70b40ae1a7579
dcarminatti <diogo.ca03@gmail.com>
2024-01-03 12:45:28 -0300

    feat:  implemented the Index function to list all user notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -4,8 +4,9 @@ const { Router } = require("express"),
 const NotesController = require("../controllers/NotesController"),
     notesController = new NotesController();
 
-notesRoutes.post("/:user_id", notesController.create);
+notesRoutes.get("/", notesController.index);
 notesRoutes.get("/:id", notesController.show);
+notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.delete("/:id", notesController.delete);
 
 module.exports = notesRoutes;
