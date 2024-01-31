1891373affd379069fc66c25df546515b568ca18
dcarminatti <diogo.ca03@gmail.com>
2024-01-31 19:29:49 -0300

    feat: implemented upload config file, created the DiskStorage class to control the file storage functions and created the route to send the user's avatar

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,6 +1,10 @@
 const { Router } = require("express"),
   usersRoutes = Router();
 
+const multer = require("multer"),
+  uploadConfig = require("../configs/upload"),
+  upload = multer(uploadConfig.MULTER);
+
 const UsersController = require("../controllers/UsersController"),
   usersController = new UsersController();
 
@@ -8,5 +12,6 @@ const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 
 usersRoutes.post("/", usersController.create);
 usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"));
 
 module.exports = usersRoutes;
