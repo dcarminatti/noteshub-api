12ee3c0c9a33835f0de588f8d657a01de964dad7
dcarminatti <diogo.ca03@gmail.com>
2024-02-02 18:58:09 -0300

    feat: configured the server to have a route that delivers the user's avatar

--- src/server.js
+++ src/server.js
@@ -3,31 +3,34 @@ require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
 const express = require("express"),
-    app = express(),
-    PORT = 3333;
+  app = express(),
+  PORT = 3333;
 
-const routes = require("./routes");
+const routes = require("./routes"),
+  uploadConfig = require("./configs/upload");
+
 migrationsRun();
 
 app.use(express.json());
 app.use(routes);
+app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use((error, req, res, next) => {
-    if (error instanceof AppError) {
-        return res.status(error.statusCode).json({
-            status: "error",
-            message: error.message,
-        });
-    }
-
-    console.error(error);
-
-    return res.status(500).json({
-        status: "error",
-        message: "Internal server error",
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
     });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`);
+  console.log(`Server is running on Port ${PORT}`);
 });
